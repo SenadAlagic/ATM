67a949734bea63d8d563c417ed3aa4dbaf4b4a93
Alagić Senad <senad.alagic@edu.fit.ba>
2022-02-05 13:42:22 +0100

    setting up the db

---
+++ ATM/ATMWinForms/Helpers/ATMdb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMWinForms.Helpers
+{
+	class ATMdb
+	{
+		public static ATMContext Baza = new ATMContext();
+	}
+}
